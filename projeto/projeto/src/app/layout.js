4345705e53e03f9894f09f23b3157b5f2880c6ba
julpereira <julia.p@estudante.ifro.edu.br>
2024-11-06 08:26:42 -0400

    commit

--- projeto/projeto/src/app/layout.js
+++ projeto/projeto/src/app/layout.js
@@ -4,6 +4,9 @@ import "./globals.css";
 export const metadata = {
   title: "Minha Primeira aplicação",
   description: "Aplicação de Front-End II",
+  charset: 'UTF-8',
+  author: 'Júlia Pereira',
+  keywords: 'HTML, CSS, JavaScript, React. Next.js'
 };
 
 export default function RootLayout({ children }) {
--- projeto/projeto/src/app/page.js
+++ projeto/projeto/src/app/page.js
@@ -2,9 +2,12 @@
 import styles from "./page.module.css";
 
 export default function Home() {
+  const nome = 'Júlia'
   return (
     <div>
       <h1>Página Principal</h1>
+      <p>Parágrafo da página principal</p>
+      <p>Autor {nome}</p>
     </div>   
   );
 }
