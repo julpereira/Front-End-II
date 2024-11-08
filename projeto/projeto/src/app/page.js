777b702d5c4789b44e6f1b8b7339e7f28a0acc78
julpereira <julia.p@estudante.ifro.edu.br>
2024-11-08 07:37:10 -0400

    commit

--- projeto/projeto/src/app/page.js
+++ projeto/projeto/src/app/page.js
@@ -1,5 +1,6 @@
 // import Image from "next/image";
 import styles from "./page.module.css";
+import Image from "next/image";
 
 export default function Home() {
   const nome = 'Júlia'
@@ -8,6 +9,7 @@ export default function Home() {
       <h1>Página Principal</h1>
       <p>Parágrafo da página principal</p>
       <p>Autor {nome}</p>
+      <Image src="/images/flor.jpg"  alt="Iamgem flor" width={600} height={400} />
     </div>   
   );
 }
