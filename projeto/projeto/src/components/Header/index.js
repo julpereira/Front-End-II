11a12bfdae40df54f38c406633b3b6b3fa0a63b9
julpereira <julia.p@estudante.ifro.edu.br>
2024-11-22 07:45:29 -0400

    up

--- projeto/projeto/src/components/Header/index.js
+++ projeto/projeto/src/components/Header/index.js
@@ -1,9 +1,15 @@
-import Link from "next/link"
-import styles from "./Header.module.css"
+'use client';
+import Link from "next/link";
+import styles from "./Header.module.css";
+import {useState} from "react";
 
 export default function Header() {
+    const [showMenu, setShowMenu] = useState (false)
     return(
         <header>
+            <button onClick={()=>setShowMenu(!showMenu)}>Clique</button>
+            {
+            showMenu &&
             <nav>
                 <ul>
                     <li><Link href='/home'></Link>Home</li>
@@ -11,6 +17,7 @@ export default function Header() {
                     <li><Link href='/contato'></Link>Contato</li>
                 </ul>
             </nav>
+            }
         </header>
     )
 }
