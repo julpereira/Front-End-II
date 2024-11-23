67f9368f431c6524ee5bfe912b06218f9e07bf48
julpereira <julia.p@estudante.ifro.edu.br>
2024-11-23 12:48:23 -0300

    up

--- projeto/projeto/src/components/Header/index.js
+++ projeto/projeto/src/components/Header/index.js
@@ -1,6 +1,3 @@
-
-import Link from "next/link";
-import styles from "./Header.module.css";
 'use client';
 import Link from "next/link";
 import styles from "./Header.module.css";
@@ -14,7 +11,7 @@ export default function Header() {
             <button onClick={()=>setShowMenu(!showMenu)}>Clique</button>
             {
             showMenu &&
-            <nav>
+            <nav>   
                 <ul>
                     <li><Link href='/home'></Link>Home</li>
                     <li><Link href='/sobre'></Link>Sobre</li>
