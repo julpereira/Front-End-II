45fda824765b0d6b28b70d8e1b877b39f20b7f56
julpereira <julia.p@estudante.ifro.edu.br>
2024-12-11 08:24:44 -0400

    commit

--- projeto/projeto/src/app/filter/page.js
+++ projeto/projeto/src/app/filter/page.js
@@ -5,7 +5,7 @@ export default function Filter() {
     const nomes = ['Wagner', 'Beatriz', 'Ana', 'Mariana', 'João'];
     const nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
     const pares = nums.filter(num => (num%2==0));
-    const [busca, setBusca] = useState
+    const [busca, setBusca] = useState('');
     console.log(nums)
     console.log(pares)
 
@@ -15,7 +15,7 @@ export default function Filter() {
             <input
                 value = {busca}
                 type="text"
-                onChange={ev => setBusca(ev.target.value)} />
+                onChange={(ev) => setBusca(ev.target.value)} />
             <ul>
                 {nomes.map((nome, i) =>(
                 <li key={i}>{i+1} - {nome}</li>
